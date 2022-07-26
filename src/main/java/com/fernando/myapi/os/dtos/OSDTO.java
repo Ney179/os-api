eb618c8df20fb9ea6d6b8427aebed976e8f9f730
Ney179 <fernando.eng.comp2019@gmail.com>
2022-07-26 16:18:27 -0300

    Ajuste na api

--- src/main/java/com/fernando/myapi/os/dtos/OSDTO.java
+++ src/main/java/com/fernando/myapi/os/dtos/OSDTO.java
@@ -6,11 +6,10 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.NotEmpty;
 
 import com.fernando.myapi.os.domain.OS;
+import com.fernando.myapi.os.domain.enums.Prioridade;
+import com.fernando.myapi.os.domain.enums.Status;
 
-import lombok.Data;
 
-
-@Data
 public class OSDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -40,6 +39,70 @@ public class OSDTO implements Serializable {
 		this.status = obj.getStatus().getCod();
 		this.tecnico = obj.getTecnico().getId();
 		this.cliente = obj.getCliente().getId();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDtaFechamento() {
+		return dtaFechamento;
+	}
+
+	public void setDtaFechamento(LocalDateTime dtaFechamento) {
+		this.dtaFechamento = dtaFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Status getStatus() {
+		return Status.toEnum(this.status);
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Integer tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Integer getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Integer cliente) {
+		this.cliente = cliente;
 	}
 }
 
